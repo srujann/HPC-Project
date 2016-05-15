86d68343e8973853a671ee403808ecb978b91e62
karanmehta93 <karanmehta93@gmail.com>
2016-05-15 08:59:22 -0700

    Switched to sequential code for finding minimum value in the last row
    Sequential code provided better performance than the optimized parallel
    code.

--- cuda_DP/seam_finder.cu
+++ cuda_DP/seam_finder.cu
@@ -8,45 +8,6 @@
 using namespace std;
 GpuTimer timer;
 
-__global__ void computeMinEnergyValue(float *last_row, int *min_idx, int width) {
-    const int bid = blockIdx.x;
-    const int tid = threadIdx.x;
-    const int pos = 2 * bid * blockDim.x + tid;
-
-    __shared__ float shared_last_row[MAX_THREADS];
-    __shared__ int shared_row_idx[MAX_THREADS];
-    
-    if((pos + blockDim.x) < width) {
-        if(last_row[pos] < last_row[pos + blockDim.x]) {
-            shared_last_row[tid] = last_row[pos];
-            shared_row_idx[tid] = pos;
-        } else {
-            shared_last_row[tid] = last_row[pos + blockDim.x];
-            shared_row_idx[tid] = pos + blockDim.x;
-        }
-    } else {
-        return;
-    }
-    
-    __syncthreads();
-    
-    for(int i = blockDim.x/2; i > 0; i = i>>1) {
-        if(tid < i) {
-            if(shared_last_row[tid] > shared_last_row[tid + i]) {
-                shared_last_row[tid] = shared_last_row[tid + i];
-                shared_row_idx[tid] = shared_row_idx[tid+i];
-            }
-        }
-        __syncthreads();
-    }
-    
-    if(tid == 0) {
-        min_idx[bid] = shared_last_row[0];
-        //printf("Min: %f\n", shared_last_row[0]);
-    }
-    
-}
-
 __global__ void computeMinEnergyMatrix(float *energy, float *min_energy, int height, int width) {
     const int bid = blockIdx.x;
     const int tid = threadIdx.x;
@@ -110,6 +71,8 @@ int main(int argc, char** argv)
         cout<<endl;
     }   
     
+    cout << "-------------------- Minimum Energy Matrix Calculation Starts --------------------" << endl;
+
     int noOfBlocks = 1; 
     int noOfThreads = min(MAX_THREADS, width);
     int sharedSize = ((N-1) / noOfThreads + 1) * noOfThreads * sizeof(float);
@@ -157,45 +120,16 @@ int main(int argc, char** argv)
     
     cout << "---------------------- Minimum Value Calculation Starts ----------------------" << endl;
     
-    noOfThreads = (width < MAX_THREADS*2) ? nextPow2((width + 1)/ 2) : MAX_THREADS;
-    noOfBlocks = (width + (noOfThreads * 2 - 1)) / (noOfThreads * 2);    
-    sharedSize = noOfBlocks * sizeof(float);
-    
-    cout<<"Blocks: "<<noOfBlocks<<"  Threads: "<<noOfThreads<<" SharedSize: "<<sharedSize<<endl;
-
-    dim3 grid_find_min(noOfBlocks), block_find_min(noOfThreads);
-    float *d_last_row;
-    int *d_min_idx;
-    int *h_min_idx = (int*) malloc(noOfBlocks * sizeof(int));
-    
-    rc = cudaMalloc((void**) &d_last_row, width * sizeof(float));
-    if(rc != cudaSuccess) {
-        cout<<"Malloc failed for last_row"<<endl;
-    }
-    
-    rc = cudaMalloc((void**) &d_min_idx, noOfBlocks * sizeof(int));
-    if(rc != cudaSuccess) {
-        cout<<"Malloc failed for min_idx"<<endl;
-    }
-    
     timer.Start();
-    // TODO: Modify for last row --> ((height-1) * width)
-    rc = cudaMemcpy(d_last_row, h_min_energy , width * sizeof(float), cudaMemcpyHostToDevice);
-    if(rc != cudaSuccess) {
-        cout<<"Memcpy failed from host to device for min_energy -> last_row"<<endl;
-    }
-    
-    
-    computeMinEnergyValue<<<grid_find_min, block_find_min>>>(d_last_row, d_min_idx, width);
-    
-    
-    rc = cudaMemcpy(h_min_idx, d_min_idx , noOfBlocks * sizeof(int), cudaMemcpyDeviceToHost);
-    if(rc != cudaSuccess) {
-        cout<<"Memcpy failed from device to host for min_idx -> min_idx "<<endl;
+    float mn = h_min_energy[0];
+    for(int i=1; i<width; i++) {
+        if(h_min_energy[i] < mn) {
+            mn = h_min_energy[i];
+        }
     }
     timer.Stop();
     cout<<"Timer Elapsed: "<<timer.Elapsed()<<endl;
-    cout<<h_min_idx[0]<<"  "<<h_min_idx[1] <<endl;
+    cout<<"mn: "<<mn<<endl;
     
     
     return 0;
