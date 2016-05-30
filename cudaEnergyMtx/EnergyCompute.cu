153c52dacd0c2cf220579d72593821875c9cc950
Anjaneya Srujan N <srujananjaneya@gmail.com>
2016-05-29 21:27:38 -0700

    Faster Parallel Energy Matrix Computation base version

--- cudaEnergyMtx/EnergyCompute.cu
+++ cudaEnergyMtx/EnergyCompute.cu
@@ -38,203 +38,212 @@ void energyCalculation(int* energy, unsigned char* data, int size, int height, i
 	}
 }
 
-void testMatrixGenerate() {
+void sequentialEnergyCompute() {
 	int size = 0, height = 0, width = 0;
-    FILE* documentRead = fopen("TestImage3", "rb");
-    if (!documentRead) {
+	FILE* documentRead = fopen("TestImage3", "rb");
+	if (!documentRead) {
 		cout << "The input document is not available" << endl;
 	}
-	
+
 	fread(&height, sizeof(int), 1, documentRead);
 	fread(&width, sizeof(int), 1, documentRead);
 	size = 3 * height * width;
-	
-	//cout << "Size:" << size << endl; 
-	unsigned char* data = new unsigned char[size]; 
+
+	unsigned char* data = new unsigned char[size];
 	fread(data, sizeof(unsigned char), size, documentRead);
 	fclose(documentRead);
-	
-	int* energy = new int[size/3];
+
+	int* energy = new int[size / 3];
 	timer.Start();
 	energyCalculation(energy, data, size, height, width);
 	timer.Stop();
-	
+
 	cout << "Time taken for serial energy computation to execute:" << timer.Elapsed() << endl;
-	
-	/*for(int i = 0; i < 9; i++) {
-		cout << data[i] << " ";
-	}
-	cout <<  endl;*/
-	
+
 	FILE* docWrite = fopen("testEnergyMatrix", "wb");
 	fwrite(&height, sizeof(int), 1, docWrite);
 	fwrite(&width, sizeof(int), 1, docWrite);
-	fwrite(energy, sizeof(int), size/3, docWrite);
+	fwrite(energy, sizeof(int), size / 3, docWrite);
 	fclose(docWrite);
-	
-	/*for(int i = 0; i < 10; i++) {
-		cout << energy[i] << " ";
-	}
-	cout <<  endl;*/
 }
 
-__global__ void energyMtxCompute(const unsigned char* imgData, int* energyMtx,
-		const int height, const int width) {
+__global__ void energyMtxCompute(const unsigned char* imgData, int* energyMtx, const int height, const int width) {
 
-	__shared__
-	unsigned char shared_imgData[3][MAX_WIDTH * 3];
-	//__shared__ int shared_energyMtx[MAX_WIDTH];
+	extern __shared__ unsigned char curr_imgData[];
 
-	int x = threadIdx.x;
-	int currIndex = 0, row = 0;
+	int tid = threadIdx.x;
+	int x = blockIdx.x * blockDim.x + tid;
+	int y = blockIdx.y;
+	int currIndex = 0;
+	int blockWidth = blockDim.x;
 
-	//load first row into shared memory
-	for(int i = x; i < 3*width; i = i+1024) {
-		shared_imgData[0][i] = imgData[i];
+	if (width < (blockIdx.x + 1) * blockDim.x) {
+		blockWidth = width - (blockIdx.x * blockDim.x);
 	}
 
-	//load the last row shared memory
-	row = (height-1)*width*3;
-	for(int i = x; i < 3*width; i = i +1024) {
-		currIndex = row + i;
-		shared_imgData[2][i] = imgData[currIndex];
+	int boundX = (y * width) * 3 + blockDim.x * blockIdx.x * 3;
+	for (int j = tid; j < 3 * blockWidth; j = j + blockWidth) {
+		curr_imgData[j] = imgData[boundX + j];
 	}
+	__syncthreads();
 
-	int hCurrent = 0, hMove = 0, hShared = 0;
-	int dx = 0, dy = 0, energyVal = 0, left = 0, right = 0, up = 0, down = 0;
+	//compute the left, right, up and down indices for current pixel
+	int left = 0, right = 0, up = 0, down = 0;
 
-	for (int y = 0; y < height; y++) {
-
-		hCurrent = y % 3;
-		hMove = (y + 1) % height;
-		hShared = (y + 1) % 3;
-
-		//move next row into shared memory
-		row = hMove * width * 3;
-		for(int i = x; i < 3*width; i = i+1024) {
-			currIndex = row + i; 
-			shared_imgData[hShared][i] = imgData[currIndex];
+	if (tid == 0) {
+		//fetch left pixel for block's left boundary
+		if (x == 0) {
+			currIndex = (y * width + width - 1) * 3;
+		} else {
+			currIndex = (y * width + x - 1) * 3;
 		}
-		__syncthreads();
 
-		for (int i = x; i < width; i = i + 1024) {
-			energyVal = 0;
-			if (i == 0) {
-				left = width - 1;
-			} else {
-				left = i - 1;
-			}
-
-			if (i == width - 1) {
-				right = 0;
-			} else {
-				right = i + 1;
-			}
-
-			if (y == 0) {
-				up = 2;
-			} else {
-				up = (y - 1) % 3;
-			}
-
-			down = (y + 1) % 3;
-
-			for (int j = 0; j < 3; j++) {
-				dx = shared_imgData[hCurrent][right * 3 + j] - shared_imgData[hCurrent][left * 3 + j];
-				dy = shared_imgData[down][i * 3 + j] - shared_imgData[up][i * 3 + j];
-				energyVal = energyVal + dx * dx + dy * dy;
-			}
-			energyMtx[y * width + i] = energyVal;
+		for (int i = 0; i < 3; i++) {
+			curr_imgData[3 * blockWidth + i] = imgData[currIndex + i];
 		}
+		left = 3 * blockWidth;
+		right = (tid + 1) * 3;
+
+	} else if (tid == blockWidth - 1) {
+		//fetch right pixel for block's right boundary
+		if (x == width - 1) {
+			currIndex = (y * width) * 3;
+		} else {
+			currIndex = (y * width + x + 1) * 3;
+		}
+
+		for (int i = 0; i < 3; i++) {
+			curr_imgData[3 * (blockWidth + 1) + i] = imgData[currIndex + i];
+		}
+
+		left = (tid - 1) * 3;
+		right = (blockWidth + 1) * 3;
+
+	} else {
+		right = (tid + 1) * 3;
+		left = (tid - 1) * 3;
 	}
+
+	if (y == 0) {
+		up = ((height - 1) * width + x) * 3;
+	} else {
+		up = ((y - 1) * width + x) * 3;
+	}
+
+	down = (((y + 1) % height) * width + x) * 3;
+
+	int energyVal = 0, dx = 0, dy = 0;
+	//compute the energy value for the pixel
+	for (int i = 0; i < 3; i++) {
+		dx = curr_imgData[right + i] - curr_imgData[left + i];
+		dy = imgData[up + i] - imgData[down + i];
+		energyVal += dx * dx + dy * dy;
+	}
+
+	//store the computed energy value to global energy matrix
+	energyMtx[y * width + x] = energyVal;
 }
 
-int main(int argc, char** argv)
-{
-	testMatrixGenerate();
+int main(int argc, char** argv) {
+	sequentialEnergyCompute();
 	int height = 0, width = 0, energyMtxSize = 0;
 	int *testEnergy;
+	string inImageFileName = "TestImage3";
 	FILE* docRead = fopen("testEnergyMatrix", "rb");
 	if (!docRead) {
 		cout << "testEnergyMatrix file is not available" << endl;
+		return 0;
 	}
-	
+
 	fread(&height, sizeof(int), 1, docRead);
 	fread(&width, sizeof(int), 1, docRead);
 	energyMtxSize = height * width;
-	
-	//cout << "Height:" << height << endl;
-	//cout << "Width:" << width << endl;
-	//cout << "Size:" << energyMtxSize << endl;
-	
-	testEnergy = (int*)malloc(energyMtxSize * sizeof(int));
+
+	testEnergy = (int*) malloc(energyMtxSize * sizeof(int));
 	fread(testEnergy, sizeof(int), energyMtxSize, docRead);
 	fclose(docRead);
-	
-	FILE* documentRead = fopen("TestImage3", "rb");
-    if (!documentRead) {
-		cout << "The input image data document is not available" << endl;
+
+	FILE* documentRead = fopen(inImageFileName.c_str(), "rb");
+	if (!documentRead) {
+		cout << "The input image file is not available" << endl;
+		return 0;
 	}
-	
+
+	cout << "Input Image read from :" << inImageFileName << endl;
+	cout << "Height of the image   :" << height << endl;
+	cout << "Width of the image    :" << width << endl;
+
 	fread(&height, sizeof(int), 1, documentRead);
 	fread(&width, sizeof(int), 1, documentRead);
 	int imgDataSize = 3 * height * width;
-	unsigned char* h_imgData = (unsigned char *)malloc(imgDataSize); 
+	unsigned char* h_imgData = (unsigned char *) malloc(imgDataSize);
 	fread(h_imgData, sizeof(unsigned char), imgDataSize, documentRead);
 	fclose(documentRead);
-	
+
 	cudaError_t rc;
-    int *d_energyMtx, *h_energyMtx;
-    unsigned char* d_imgData;
-    
-    h_energyMtx = (int *)malloc(energyMtxSize * sizeof(int));
-    
-    rc = cudaMalloc((void**) &d_energyMtx, energyMtxSize * sizeof(int));
-    if(rc != cudaSuccess) {
-        cout<<"Malloc Failed for d_energyMtx"<<endl;
-    }
-    
-    rc = cudaMalloc((void**) &d_imgData,  imgDataSize);
-    if(rc != cudaSuccess) {
-        cout<<"Malloc Failed for d_imgData"<<endl;
-    }
-    
-    rc = cudaMemcpy(d_imgData, h_imgData, imgDataSize, cudaMemcpyHostToDevice);
-    if(rc != cudaSuccess) {
-        cout<<"Memcpy failed from host to device"<<endl;
-    }
-	
-	dim3 gridDim(1, 1, 1);
-	dim3 blockDim(1024, 1, 1);
-	
-    timer.Start();
-    energyMtxCompute<<<gridDim, blockDim>>>(d_imgData, d_energyMtx, height, width);
+	int *d_energyMtx, *h_energyMtx;
+	unsigned char* d_imgData;
+
+	h_energyMtx = (int *) malloc(energyMtxSize * sizeof(int));
+
+	rc = cudaMalloc((void**) &d_energyMtx, energyMtxSize * sizeof(int));
+	if (rc != cudaSuccess) {
+		cout << "Malloc Failed for d_energyMtx" << endl;
+	}
+
+	rc = cudaMalloc((void**) &d_imgData, imgDataSize);
+	if (rc != cudaSuccess) {
+		cout << "Malloc Failed for d_imgData" << endl;
+	}
+
+	rc = cudaMemcpy(d_imgData, h_imgData, imgDataSize, cudaMemcpyHostToDevice);
+	if (rc != cudaSuccess) {
+		cout << "Memcpy failed from host to device" << endl;
+	}
+
+	int numThreads = min(MAX_THREADS, width);
+	int gridRows = 1;
+	int gridCols = height;
+	int sharedSize = (numThreads + 2) * 3;
+
+	if (width % numThreads == 0) {
+		gridRows = width / numThreads;
+	} else {
+		gridRows = width / numThreads + 1;
+	}
+
+	dim3 gridDim(gridRows, gridCols, 1);
+	dim3 blockDim(numThreads, 1, 1);
+
+	timer.Start();
+	energyMtxCompute<<<gridDim, blockDim, sharedSize>>>(d_imgData, d_energyMtx, height, width);
 	timer.Stop();
-    
-    rc = cudaMemcpy(h_energyMtx, d_energyMtx, energyMtxSize * sizeof(int), cudaMemcpyDeviceToHost);
-    if(rc != cudaSuccess) {
-        cout<<"Memcpy failed from device to host with rc:"<< rc << endl;
-    }   
-	
+
+	rc = cudaMemcpy(h_energyMtx, d_energyMtx, energyMtxSize * sizeof(int), cudaMemcpyDeviceToHost);
+	if (rc != cudaSuccess) {
+		cout << "Memcpy failed from device to host with rc:" << rc << endl;
+	}
+
 	cout << "Time taken for parallel energy computation to execute:" << timer.Elapsed() << endl;
-	int  i = 0;
-	for(i = 0; i < height*width; i++) {
-		if(testEnergy[i] != h_energyMtx[i]) {
-			cout << "error at index i:" << i << "|expected:" << testEnergy[i] << "|actual:" << h_energyMtx[i] << endl;  
+	int i = 0;
+	for (i = 0; i < height * width; i++) {
+		if (testEnergy[i] != h_energyMtx[i]) {
+			cout << "error at index i:" << i << "|expected:" << testEnergy[i] << "|actual:" << h_energyMtx[i] << endl;
+			break;
 		}
 	}
-	
-	if(i == height*width) {
+
+	if (i == height * width) {
 		cout << "Success" << endl;
 	} else {
 		cout << "Failure" << endl;
 	}
-	
+
 	cudaFree(d_energyMtx);
 	cudaFree(d_imgData);
 	free(h_energyMtx);
 	free(h_imgData);
 	free(testEnergy);
 
-    return 0;
+	return 0;
 }
